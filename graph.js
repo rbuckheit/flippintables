b0388aa98cc3291b8b90b939628af013852e5af7
Ryan Buckheit <rbuckheit@gmail.com>
2012-03-03 20:16:20 -0800

    allow user to turn graph on and off via ctrl+shift+g

--- graph.js
+++ graph.js
@@ -4,7 +4,19 @@ $(document.body).append("<div id=\"ftables_graph\"></div>")
 
 // bind hotkeys
 
-$(document).bind("keydown", "ctrl+g", function() {
-  $("#ftables_graph").css("display","block");
-  return false;
+var graphOn = false;
+
+//TODO graph fade effects
+shortcut.add("Ctrl+Shift+G",function() {
+  console.log("GRAPH HOTKEY");
+	graph = $("#ftables_graph");
+	console.log(graph.css("display"));
+  if (graphOn)  {
+    console.log("disable graph");
+    graph.css("display", "none"); // graph.fadeOut(1500, 'linear', function() {});
+  } else {
+    console.log("enable graph");
+    graph.css("display", "block");
+  }
+  graphOn = !graphOn;
 });
