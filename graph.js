3e47178b4c2f2869631987a5b99e176bcf501ff4
Cai GoGwilt <gogwilt@gmail.com>
2012-03-03 23:31:15 -0800

    Integrates the graph with the chrome extension

--- graph.js
+++ graph.js
@@ -2,26 +2,35 @@
 
 $(document.body).append("<div id=\"ftables_graph\"><p>Hi</p></div>")
 
+// browser graph stuff
+var browserGraph;
+// add Svg Stuff
+  var svg = d3.select("#ftables_graph")
+            .append("svg:svg")
+            .attr("class", "browser-graph")
+            .attr("width", 960)
+            .attr("height", 500);
+            
+    var selection = new GraphSelection(svg);
+    browserGraph = new BrowserGraph(svg);
+    
+    selection.onSelection(function() {
+      var lastrect = selection.getLastRect();
+      browserGraph.select(function(d) {
+        return (d.x > lastrect.x && d.x < lastrect.x + lastrect.width && d.y > lastrect.y && d.y < lastrect.y + lastrect.height);
+      });
+    });
+
+
 // bind hotkeys
 
 var graphOn = false;
 
-//TODO graph fade effects
-shortcut.add("Ctrl+Shift+G",function() {
-
-	graph = $("#ftables_graph");
-
-  if (graphOn)  {
-    graph.css("display", "none"); // graph.fadeOut(1500, 'linear', function() {});
-  } else {    
-    $("#ftables_graph").html(""); 
-    
+function loadNodes() {
     var nodes = [];
     chrome.extension.sendRequest({cmd: "get_nodes"}, function(response) {
       nodes = response.nodes;
       addNodes(nodes);
-    });
-    
     console.log("getting links");
     var links = [];
     chrome.extension.sendRequest({cmd: "get_links"}, function(response) {
@@ -31,16 +40,57 @@ shortcut.add("Ctrl+Shift+G",function() {
       }
       addLinks(links);
     });
+    });
+}
+
+loadNodes();
+
+//TODO graph fade effects
+shortcut.add("Ctrl+Shift+G",function() {
+
+	graph = $("#ftables_graph");
+
+  if (graphOn)  {
+    graph.css("display", "none"); // graph.fadeOut(1500, 'linear', function() {});
+  } else {    
+//    $("#ftables_graph").html("");     
     
     graph.css("display", "block");
+
+    // loadNodes();
   }
   graphOn = !graphOn;
 });
 
+function convertLinks(links) {
+  formatted = [];
+  links.forEach(function (l) {
+    var f = {};
+    f.source = l.src;
+    f.target = l.dest;
+    formatted.push(f);
+  });
+  return formatted;
+}
+
 function addLinks(links) {
-  $("#ftables_graph").append("<p> LOADED LINKS </p> <pre>" +  JSON.stringify(links) + "</pre>");
+  browserGraph.addLink(convertLinks(links));
+  // $("#ftables_graph").append("<p> LOADED LINKS </p> <pre>" +  JSON.stringify(links) + "</pre>");
+}
+
+function convertNodes(nodes) {
+  formatted = [];
+  nodes.forEach(function (l) {
+    var f = {};
+    f.title = l.title;
+    f.image = l.favicon;
+    formatted.push(f);
+  });
+  console.log(formatted);
+  return formatted;
 }
 
 function addNodes(nodes) {
-  $("#ftables_graph").append("<p> LOADED NODES </p> <pre>" + array_to_str(nodes) + "</pre>")
+  browserGraph.addNode(convertNodes(nodes));
+  // $("#ftables_graph").append("<p> LOADED NODES </p> <pre>" + array_to_str(nodes) + "</pre>")
 }
